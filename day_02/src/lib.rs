1f9f1804a290d6554f3e855ad29c18b3c87f1780
Olof Larsson <olof@folof.se>
2021-12-02 08:26:43 +0100

    feat: add day_02 part two

--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -14,18 +14,40 @@ pub fn movement(lines:Vec<String>)->i32 {
 			"down" => y += speed,
 			_ => println!("defualt?"),
 		}
-			
+	}
+
+	return x * y
+}
+
+pub fn aim(lines:Vec<String>)->i32 {
+	let mut x:i32 = 0;
+	let mut y:i32 = 0;
+	let mut aim:i32 = 0;
+
+	for line in lines {
+		let strSplit = line.split_whitespace().collect::<Vec<&str>>();
+		let direction:&str = strSplit[0];
+		let speed:i32 = strSplit[1].parse::<i32>().unwrap();
+
+		match direction {
+			"forward" => {
+				x += speed;
+				y += aim * speed;
+			},
+			"up" => aim -= speed,
+			"down" => aim += speed,
+			_ => println!("defualt?"),
+		}
 	}
 
 	return x * y
 }
 
 
-
-
 #[cfg(test)]
 mod tests {
 	use crate::movement;
+	use crate::aim;
 	use shared::read_lines;
 
 	#[test]
@@ -40,7 +62,7 @@ mod tests {
 	}
 
 	#[test]
-
+	#[ignore]
 	fn movement_test2() {
 		let lines = read_lines("./src/input.txt")
 			.iter()
@@ -48,6 +70,27 @@ mod tests {
 			.collect();
 
 		assert_eq!(movement(lines), 1499229);
+	}
+
+	#[test]
+	#[ignore]
+	fn aim_test() {
+		let lines = read_lines("./src/example.txt")
+			.iter()
+			.map(|f| f.parse().unwrap())
+			.collect();
+
+		assert_eq!(aim(lines), 900);
+	}
+
+	#[test]
+	fn aim_test2() {
+		let lines = read_lines("./src/input.txt")
+			.iter()
+			.map(|f| f.parse().unwrap())
+			.collect();
+
+		assert_eq!(aim(lines), 1340836560);
 	}
    
 }
